4cbb85a5122db6f7cf929e7505af7a02c364fa63
alvarobem <alvaroberrocal12@gmail.com>
2023-04-26 16:12:32 +0200

    Added initial data load into db

--- src/main/resources/db/migration/V1_1_0__prices_table.sql
+++ src/main/resources/db/migration/V1_1_0__prices_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE inditex_code_challenge.prices (
     id int NOT NULL AUTO_INCREMENT,
     start_date datetime NOT NULL,
     end_date datetime NOT NULL,
-    price int NOT NULL,
+    price decimal NOT NULL,
     price_list int NOT NULL,
     product_id bigint NOT NULL,
     brand_id bigint NOT NULL,
